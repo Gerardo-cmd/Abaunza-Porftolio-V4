28e3dac1598e0620b2c73a4bdeded218cd0a7703
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-05-30 11:22:23 -0400

    Finished navbar for real, added image in about page, and added work experience page

--- my-app/src/pages/WorkExperience/WorkExperiencePage.js
+++ my-app/src/pages/WorkExperience/WorkExperiencePage.js
@@ -7,9 +7,22 @@ let WorkExperiencePage = () => {
                 <NavBar location="workExperience"/>
                 <div className="container" id="main-content">
                     <h2>Internships</h2>
+                    <h4>CMTSolutions - Software Engineer Intern</h4>
+                    <h5>Date worked: 05/17/2021 - Present</h5>
+                    <ul>
+                        <li>Developed and supported web applications using Ruby on Rails and React</li>
+                        <li>Used code enabling tools such as Github and Jira to view everyone's progress and perform code reviews</li>
+                        <li>Took part in meetings with stakeholders to see how development expectations are defined for projects from the outset</li>
+                        <li>Joined in all developer activities, including iteration planning, design, development, and deployment for mission critical applications and upgrades</li>
+                    </ul>
                     <p></p>
                     <br />
                     <h2>Freelance</h2>
+                    <h4>GE Electric</h4>
+                    <ul>
+                        <li>Developed a web application for GE Electric using React and Hooks</li>
+                        <li>Took part in weekly meetings with client to review customer requirements and discuss progress</li>
+                    </ul>
                 </div>
             </div>
         </div>

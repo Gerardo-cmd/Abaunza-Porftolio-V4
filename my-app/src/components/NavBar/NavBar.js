d60c5145de08c428086dd91884405b7e1933e665
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-05-31 20:17:43 -0400

    Fixing navbar href

--- my-app/src/components/NavBar/NavBar.js
+++ my-app/src/components/NavBar/NavBar.js
@@ -11,18 +11,18 @@ let NavBar = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link variant="outline-info" href="/">Home</Nav.Link>
-                    <Nav.Link variant="outline-info" href="/about">About</Nav.Link>
-                    <Nav.Link variant="outline-info" href="/projects">Projects</Nav.Link>
+                    <Nav.Link variant="outline-info" href="./">Home</Nav.Link>
+                    <Nav.Link variant="outline-info" href="./about">About</Nav.Link>
+                    <Nav.Link variant="outline-info" href="./projects">Projects</Nav.Link>
                     <Nav.Link href="/history">History</Nav.Link>
                     <NavDropdown title="Experience" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="/skills">Skills</NavDropdown.Item>
-                        <NavDropdown.Item href="/courses">Courses</NavDropdown.Item>
-                        <NavDropdown.Item href="/workExperience">Work Experience</NavDropdown.Item>
+                        <NavDropdown.Item href="./skills">Skills</NavDropdown.Item>
+                        <NavDropdown.Item href="./courses">Courses</NavDropdown.Item>
+                        <NavDropdown.Item href="./workExperience">Work Experience</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 <Nav className="right">
-                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link href="./contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>

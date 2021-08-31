83811de045174ac6c9f1d4bfc3c4578e47ac86a9
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-08-30 23:26:22 -0400

    Updated skills and work experience

--- my-app/src/pages/Projects/ProjectsPage.js
+++ my-app/src/pages/Projects/ProjectsPage.js
@@ -37,7 +37,7 @@ let ProjectsPage = () => {
                     <div className="row" id = "Todo App">
                         <div className="col-lg">
                             <h2>Todo App</h2>
-                            <p>The user can add todos and remove them as they wish. There are checkboxes next to each todo 
+                            <p>The old classic 'Todo App'! The user can add todos and remove them as they wish. There are checkboxes next to each todo 
                                 that the user can check to signify if it is completed or not. At the top of the website one
                                 can find a search bar that will filter the todos based on what the user types in by looking for 
                                 a matching title. The user will also be told how many todos are left based on how many unfinished 
